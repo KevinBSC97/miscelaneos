328113d9521e9b1ac913556697338f93fadb4991
KevinBSC97 <kevinsotomayor1@hotmail.com>
2024-02-25 17:18:27 -0500

    se agregaron las pantallas de los sensores al archivo de barril

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,2 +1,8 @@
 export 'home/home_screen.dart';
 export 'permissions/permissions_screen.dart';
+
+export 'sensors/acelerometro_screen.dart';
+export 'sensors/brujula_screen.dart';
+export 'sensors/giroscopio_ball_screen.dart';
+export 'sensors/giroscopio_screen.dart';
+export 'sensors/magnetometro_screen.dart';
