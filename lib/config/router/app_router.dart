d13c616e2e2912e8a661187db6f7bc025f47e97d
KevinBSC97 <kevinsotomayor1@hotmail.com>
2024-02-25 17:16:17 -0500

    se agrega laruta para los tipos de sensores

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -3,6 +3,7 @@ import 'package:miscelaneos/presentation/screens/screens.dart';
 
 final router = GoRouter(
   routes: [
+    //! General
     GoRoute(
       path: '/',
       builder: (context, state) => const HomeScreen(),
@@ -10,6 +11,27 @@ final router = GoRouter(
     GoRoute(
       path: '/permissions',
       builder: (context, state) => const PermissionsScreen(),
-    )
+    ),
+    //!Sensors
+    GoRoute(
+      path: '/accelerometer',
+      builder: (context, state) => const AcelerometroScreen(),
+    ),
+    GoRoute(
+      path: '/compass',
+      builder: (context, state) => const BrujulaScreen(),
+    ),
+    GoRoute(
+      path: '/gyroscope-ball',
+      builder: (context, state) => const GiroscopioBallScreen(),
+    ),
+    GoRoute(
+      path: '/gyroscope',
+      builder: (context, state) => const GiroscopioScreen(),
+    ),
+    GoRoute(
+      path: '/magnetometer',
+      builder: (context, state) => const MagnetometroScreen(),
+    ),
   ]
 );
