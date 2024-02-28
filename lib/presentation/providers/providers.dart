89085e82b96060794eed4fe2a5db871f63f4f689
KevinBSC97 <kevinsotomayor1@hotmail.com>
2024-02-27 20:56:13 -0500

    se agreo los providers al archivo de barril

--- lib/presentation/providers/providers.dart
+++ lib/presentation/providers/providers.dart
@@ -2,3 +2,7 @@ export 'app_state_provider.dart';
 export 'permissions/permissions_provider.dart';
 
 export 'sensors/giroscopio_provider.dart';
+export 'sensors/acelerometro_providers.dart';
+export 'sensors/magnetometro_provider.dart';
+
+export 'compass/compass_provider.dart';
