dc49cefc1be0fe662d0b1947373d4103fa606cd9
Kevin <kevinsotomayor1@hotmail.com>
2025-02-04 22:35:04 -0500

    ruta para los biometricos

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,39 +1,38 @@
 import 'package:go_router/go_router.dart';
 import 'package:miscelaneos/presentation/screens/screens.dart';
 
-final router = GoRouter(
-  routes: [
-    //! General
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const HomeScreen(),
-    ),
-    GoRoute(
-      path: '/permissions',
-      builder: (context, state) => const PermissionsScreen(),
-    ),
-    //!Sensors
-    GoRoute(
-      path: '/accelerometer',
-      builder: (context, state) => const AcelerometroScreen(),
-    ),
-    GoRoute(
-      path: '/compass',
-      builder: (context, state) => const BrujulaScreen(),
-    ),
-    GoRoute(
-      path: '/gyroscope-ball',
-      builder: (context, state) => const GiroscopioBallScreen(),
-    ),
-    GoRoute(
-      path: '/gyroscope',
-      builder: (context, state) => const GiroscopioScreen(),
-    ),
-    GoRoute(
-      path: '/magnetometer',
-      builder: (context, state) => const MagnetometroScreen(),
-    ),
-    GoRoute(
+final router = GoRouter(routes: [
+  //! General
+  GoRoute(
+    path: '/',
+    builder: (context, state) => const HomeScreen(),
+  ),
+  GoRoute(
+    path: '/permissions',
+    builder: (context, state) => const PermissionsScreen(),
+  ),
+  //!Sensors
+  GoRoute(
+    path: '/accelerometer',
+    builder: (context, state) => const AcelerometroScreen(),
+  ),
+  GoRoute(
+    path: '/compass',
+    builder: (context, state) => const BrujulaScreen(),
+  ),
+  GoRoute(
+    path: '/gyroscope-ball',
+    builder: (context, state) => const GiroscopioBallScreen(),
+  ),
+  GoRoute(
+    path: '/gyroscope',
+    builder: (context, state) => const GiroscopioScreen(),
+  ),
+  GoRoute(
+    path: '/magnetometer',
+    builder: (context, state) => const MagnetometroScreen(),
+  ),
+  GoRoute(
       path: '/pokemons',
       builder: (context, state) => const PokemonsScreen(),
       routes: [
@@ -44,7 +43,9 @@ final router = GoRouter(
             return PokemonScreen(pokemonId: id);
           },
         )
-      ]
-    )
-  ]
-);
+      ]),
+  GoRoute(
+    path: '/biometrics',
+    builder: (context, state) => const BiometricScreen(),
+  )
+]);
