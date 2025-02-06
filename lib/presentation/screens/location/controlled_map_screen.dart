c3dda24e3b69af146520de950d01987cd2e1787b
Kevin <kevinsotomayor1@hotmail.com>
2025-02-06 15:06:53 -0500

    pantalla para el control del mapa y marcadores

--- lib/presentation/screens/location/controlled_map_screen.dart
+++ lib/presentation/screens/location/controlled_map_screen.dart
@@ -1,15 +1,115 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:miscelaneos/presentation/providers/providers.dart';
 
 //Pantalla para tener un control del mapa, agregar marcadores, mover la toma del mapa, etc
-class ControlledMapScreen extends StatelessWidget {
+class ControlledMapScreen extends ConsumerWidget {
   const ControlledMapScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
+    final watchUserLocation = ref.watch(watchLocationProvider);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Controlled Map Screen'),
-      ),
+      body: watchUserLocation.when(
+          data: (data) => MapAndControls(latitude: data.$1, longitude: data.$2),
+          error: (error, stackTrace) => Text('$error'),
+          loading: () => const Center(
+                child: Text('Ubicando usuario'),
+              )),
+    );
+  }
+}
+
+class MapAndControls extends ConsumerWidget {
+  final double latitude;
+  final double longitude;
+  const MapAndControls(
+      {super.key, required this.latitude, required this.longitude});
+
+  @override
+  Widget build(BuildContext context, ref) {
+    final mapControllerState = ref.watch(mapControllerProvider);
+    return Stack(
+      children: [
+        _MapView(initialLat: latitude, initialLng: longitude),
+        //Boton para salir de la pantalla
+        Positioned(
+            top: 40,
+            left: 20,
+            child: IconButton.filledTonal(
+                onPressed: () {
+                  context.pop();
+                },
+                icon: const Icon(Icons.arrow_back))),
+        //Boton para ir a la posicion del usuario
+        Positioned(
+            top: 40,
+            left: 20,
+            child: IconButton.filledTonal(
+                onPressed: () {
+                  ref.read(mapControllerProvider.notifier).findUser();
+                },
+                icon: const Icon(Icons.location_searching))),
+        //Seguir usuario
+        Positioned(
+            top: 90,
+            left: 20,
+            child: IconButton.filledTonal(
+                onPressed: () {
+                  ref.read(mapControllerProvider.notifier).toggleFollowUser();
+                },
+                icon: Icon(mapControllerState.followUser
+                    ? Icons.directions_run
+                    : Icons.accessibility_new_outlined))),
+        //Crear marcador
+        Positioned(
+            top: 140,
+            left: 20,
+            child: IconButton.filledTonal(
+                onPressed: () {
+                  ref
+                      .read(mapControllerProvider.notifier)
+                      .addMarkerCurrentPosition();
+                },
+                icon: const Icon(Icons.pin_drop))),
+      ],
+    );
+  }
+}
+
+class _MapView extends ConsumerStatefulWidget {
+  final double initialLat;
+  final double initialLng;
+
+  const _MapView({required this.initialLat, required this.initialLng});
+
+  @override
+  __MapViewState createState() => __MapViewState();
+}
+
+class __MapViewState extends ConsumerState<_MapView> {
+  @override
+  Widget build(BuildContext context) {
+    final mapController = ref.watch(mapControllerProvider);
+    return GoogleMap(
+      markers: mapController.markersSet,
+      mapType: MapType.normal,
+      initialCameraPosition: CameraPosition(
+          target: LatLng(widget.initialLat, widget.initialLng), zoom: 12),
+      myLocationEnabled: true,
+      zoomControlsEnabled: false,
+      myLocationButtonEnabled: false,
+      onMapCreated: (GoogleMapController controller) {
+        //_controller.complete(controller);
+        ref.read(mapControllerProvider.notifier).setMapController(controller);
+      },
+      onLongPress: (latLng) {
+        ref
+            .read(mapControllerProvider.notifier)
+            .addMarker(latLng.latitude, latLng.longitude, 'Custom Marker');
+      },
     );
   }
 }
